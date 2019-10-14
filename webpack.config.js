03a4dc442007a9fe1ceb07dd8fb8c19887dd45c0
michael.liebhauser <mail@viduct.de>
2019-10-14 12:04:26 +0200

    Removed unused packages Renamed dist folder to docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   plugins: [
